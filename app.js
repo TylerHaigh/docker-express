d1c7d9e28ee5b89514868786428a70fe6fb9394b
Tyler Haigh <tylerhaigh1994@me.com>
2019-09-01 11:43:47 +1000

    Add healthcheck endpoint

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Healthcheck endpoint
+// https://stackoverflow.com/questions/43380939/where-does-the-convention-of-using-healthz-for-application-health-checks-come-f
+app.get('/healthz', (req, res) => {
+    res.send('OK');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = {app};
